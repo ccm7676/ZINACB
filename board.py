1fd9433201bb6ed99c0114d5134e421ef662e145
Carl Marino <carlcmarino@protonmail.com>
2021-09-04 14:03:34 +0200

    started a possble moves system

--- board.py
+++ board.py
@@ -3,10 +3,10 @@ class Board():
 	
 	board = [["r", "n", "b", "q", "k", "b", "n", "r"], # black pieces
 			["p", "p", "p", "p", "p", "p", "p", "p"], # black pawns
-			["nul", "nul", "nul", "nul", "nul", "nul", "nul", "nul"], # empty
-			["nul", "nul", "nul", "nul", "nul", "nul", "nul", "nul"], # empty
-			["nul", "nul", "nul", "nul", "nul", "nul", "nul", "nul"], # empty
-			["nul", "nul", "nul", "nul", "nul", "nul", "nul", "nul"], # empty
+			["null", "null", "null", "null", "null", "null", "null", "null"], # empty
+			["null", "null", "null", "null", "null", "null", "null", "null"], # empty
+			["null", "null", "null", "null", "null", "null", "null", "null"], # empty
+			["null", "null", "null", "null", "null", "null", "null", "null"], # empty
 			["P", "P", "P", "P", "P", "P", "P", "P"], # white pawns
 			["R", "N", "B", "Q", "K", "B", "N", "R"]] # white pieces
 			
@@ -19,10 +19,25 @@ class Board():
 			print("\n")
 
 			
-			
-			
-		pass
-		
+	def possible_moves(self, x,y):
+		if(self.board[y][x].lower() == "p"):
+			if(self.board[y+1][x] == "null"):
+				print((x,y+1))
+			if(self.board[y+1][x+1].isupper() == False):
+				print((x+1,y+1))
+			if(self.board[y+1][x-1].isupper() == False):
+				print((x-1,y+1))
+
+		elif(self.board[y][x].lower() == "k"):
+			print("king")
+		elif(self.board[y][x].lower() == "q"):
+			print("queen")
+		elif(self.board[y][x].lower() == "b"):
+			print("bishop")
+		elif(self.board[y][x].lower() == "n"):
+			print("knight")
+		elif(self.board[y][x].lower() == "r"):
+			print("rook")
 	
 			
 	
@@ -31,3 +46,7 @@ class Board():
 b = Board()
 
 b.print_board()
+
+while True:
+	inp = input("x, y:").split(",")
+	b.possible_moves(int(inp[0]),int(inp[1]))
