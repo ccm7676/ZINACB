355da6f8156aa204989a4118a3a2a60d6bfa86fc
Itay G <itaykag@gmail.com>
2021-09-05 10:49:56 -0700

    delete files

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 # game handling 
-import brain
 import board
 
 
 class Game():
 	
-	def __init__(self, game_board, game_brain):
+	def __init__(self, game_board):
 		self.game_board = game_board 
 		self.game_brain = game_brain
-		
-	def start_game(self):
-		self.game_board.print_board()
-		inp = input("pos (chess notation): ")
-			
-		
-		print(self.game_brain.gen_possible_moves(inp, self.game_board))
-		self.game_board.print_board()
 
 
 b = board.Board()
-br = brain.Zinca("standard")
-g = Game(b, br)
+g = Game(b)
 
 
 g.start_game()
